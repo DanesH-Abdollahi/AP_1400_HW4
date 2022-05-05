718c57ea1674bf9cae27dc2b89f242c9dc4f5de7
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2022-05-05 19:56:50 +0430

    Code's R Editted

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,6 @@ int main(int argc, char** argv)
 {
     if (false) // make false to run unit-tests
     {
-        UniquePtr<int> ptr1 { new int { 10 } };
-        // static_assert(false, "You Can't Use Copy Constructor For an Object Of UniquePtr Class");
-        // UniquePtr<int> ptr2 { ptr1 };
 
     } else {
         ::testing::InitGoogleTest(&argc, argv);
